e746d34bd3f53080e92b8f9750ce509bc755ed12
IthielGit <harinosy.tradeco@gmail.com>
2022-07-13 19:13:45 +0300

    footer v2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdb-react-ui-kit";
 
 import { ImLocation2 } from "react-icons/im";
 import { BsFillTelephoneFill } from "react-icons/bs";
-import { MdEmail,MdAddLocationAlt } from "react-icons/md";
+import { MdEmail, MdAddLocationAlt } from "react-icons/md";
 
 import { images } from "../../constants";
 
@@ -11,40 +11,52 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <MDBFooter
-      style={{ backgroundColor: "var(--color-black)" }}
-      className="font-small pt-4 mt-4"
-    >
-      <MDBContainer fluid className="text-center text-md-left">
-        <MDBRow className="app__footer">
-          <MDBCol className="app__footer-col_1" md="4">
-              <img src={images.logo} alt="Logo" />
-          </MDBCol>
-          <MDBCol className="app__footer-col_2" md="4">
-            <h5 className="title">Liens utiles</h5>
-              <ul className="app__footer-links">
-                <li className="p__inter" style={{color: 'white'}}><a href="#home">Accueil</a></li>
-                <li className="p__inter" style={{color: 'white'}} ><a href="#about">A propos de nous</a></li>
-                <li className="p__inter" style={{color: 'white'}} ><a href="#contact">Contact</a></li>
+    <footer>
+      <div className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className=" border_top1"></div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <h3>menu LINKS</h3>
+              <ul className="link_menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="about.html"> About</a></li>
+                <li><a href="product.html">Our Product</a></li>
+                <li><a href="gallery.html">Gallery</a></li>
+                <li><a href="contact.html">Contact Us</a></li>
               </ul>
-          </MDBCol>
-          <MDBCol className="app__footer-col_3" md="4">
-            <h5 className="title">Coordonées</h5>
-            <ul className="app__footer-list">
-              <li> <ImLocation2/>Rue 20 Faravohitra</li>
-              <li> <MdAddLocationAlt/>Antananarivo Madagscar 101</li>
-              <li> <BsFillTelephoneFill/>+34 26 555 15</li>
-              <li> <MdEmail/>seekngo@seekngo.com</li>
-            </ul>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-      <div className="app__footer-copyright text-center py-3">
-        <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright {"SeeknGo"}
-        </MDBContainer>
+            </div>
+            <div className=" col-md-3">
+              <h3>TOP food</h3>
+              <p className="many">
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected
+              </p>
+            </div>
+            <div className="col-lg-3 offset-mdlg-2     col-md-4 offset-md-1">
+              <h3>Contact </h3>
+              <ul className="conta">
+                <li><i className="fa fa-map-marker" aria-hidden="true"></i> Location</li>
+                <li> <i className="fa fa-envelope" aria-hidden="true"></i><a href="#"> demo@gmail.com</a></li>
+                <li><i className="fa fa-mobile" aria-hidden="true"></i> Call : +01 1234567890</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="copyright">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-10 offset-md-1">
+                <p>© 2019 All Rights Reserved. Design by <a href="https://html.design/"> Free Html Templates</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </MDBFooter>
+    </footer>
   );
 }
 
