b7bc5f549eb910efa6fd0301f49c9e88069c1acb
ChrysR12 <chrys.rakoto@gmail.com>
2022-08-11 10:07:19 +0300

    changement nom partenaire

--- src/container/SignUp/SignUp.jsx
+++ src/container/SignUp/SignUp.jsx
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import { Footer, Navbar, SubHeading } from "../../components";
-import { data } from '../../constants'
+import { data } from "../../constants";
 
-import './SignUp.css'
+import "./SignUp.css";
 
 function SignUp() {
-
   const [accountType, setAccountType] = useState(null);
 
   const [nifContentVisible, setNifContentVisible] = useState(false);
 
   useEffect(() => {
-    accountType === "partenaire" ? setNifContentVisible(true) : setNifContentVisible(false);
+    accountType === "partenaire"
+      ? setNifContentVisible(true)
+      : setNifContentVisible(false);
   }, [accountType]);
 
   const handleChange = (e) => {
     setAccountType(e.target.value);
-  }
+  };
 
   return (
     <>
@@ -31,36 +32,56 @@ function SignUp() {
                 <div className="col-lg-6 col-md-8 mx-auto">
                   <div className="signup-form">
                     <form action="">
-
-                      <select className="form-select" id="accountType" value={accountType} onChange={handleChange} required>
+                      <select
+                        className="form-select"
+                        id="accountType"
+                        value={accountType}
+                        onChange={handleChange}
+                        required
+                      >
                         <option selected>Sélectionner le type de compte</option>
                         <option value="client">Client</option>
                         <option value="partenaire">Partenaire</option>
                       </select>
-                      <div className='app__signup_inputs__inline'>
-                        <input className="signup__input" type="text" placeholder="Prénom" />
-                        <input type="text" placeholder="Nom" />
-                      </div>
-                      <div>
-                        <input type="text" placeholder="Numéro de telephone" />
-                      </div>
-                      <div className='app__signup_inputs__inline'>
-                        <input className="signup__input" type="text" placeholder="Adresse" />
-                        <select className="form-select" required>
-                          <option>Province</option>
-                          {data.provinces.map((province, index) => (
-                            <option key={index} value={province.title}>{province.title}</option>
-                          ))}
-                        </select>
-                      </div>
 
-                      {nifContentVisible &&
+                      {!nifContentVisible && (
+                        <div className="app__signup_inputs__inline">
+                          <input
+                            className="signup__input"
+                            type="text"
+                            placeholder="Prénom"
+                          />
+                          <input type="text" placeholder="Nom" />
+                        </div>
+                      )}
+
+                      {nifContentVisible && (
                         <div>
+                          <input type="text" placeholder="Nom / Nom société" />
                           <input type="text" placeholder="Numéro NIF" />
                           <input type="text" placeholder="Numéro STAT" />
                           <input type="text" placeholder="Numéro RCS" />
                         </div>
-                      }
+                      )}
+
+                      <div>
+                        <input type="text" placeholder="Numéro de telephone" />
+                      </div>
+                      <div className="app__signup_inputs__inline">
+                        <input
+                          className="signup__input"
+                          type="text"
+                          placeholder="Adresse"
+                        />
+                        <select className="form-select" required>
+                          <option>Province</option>
+                          {data.provinces.map((province, index) => (
+                            <option key={index} value={province.title}>
+                              {province.title}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
                       <div>
                         <input type="email" placeholder="Email" />
                       </div>
@@ -79,12 +100,10 @@ function SignUp() {
             </div>
           </div>
         </section>
-
       </div>
       <Footer />
     </>
-
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
